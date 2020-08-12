0b2e266cf39b6af581aafa7140e52139904dfdb4
海老名翔平 <ebina.shohei.0613@gmail.com>
2020-08-12 21:44:02 +0900

    実践課題終了

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  post "/callback" => "line_bot#callback"
+  resources :tasks
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
